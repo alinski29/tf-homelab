c92ccd216de40326770beeeb0106e4e9b98f2f7f
Alinski <alinsecareanu@protonmail.com>
2025-03-31 17:54:19 +0300

    remove redundant comments

--- tranmsission.tf
+++ tranmsission.tf
@@ -48,21 +48,3 @@ resource "docker_container" "transmission" {
     container_path = "/watch"
   }
 }
-
-# resource "docker_container" "ollama" {
-#   # provider = docker # Default provider (localhost)
-#   name    = "ollama"
-#   image   = "ollama/ollama:${var.ollama_docker_tag}"
-#   restart = "unless-stopped"
-#   tty     = true # Keep TTY attached as per compose
-
-#   # pull_policy = "always" # Terraform docker provider manages image pulling implicitly
-
-#   volumes {
-#     # Assuming DOCKER_VOLUMES_HOME on localhost might be different,
-#     # adjust var.docker_volumes_home default or override if needed.
-#     # For now, using the same variable.
-#     host_path      = "${var.docker_volumes_home}/ollama"
-#     container_path = "/root/.ollama"
-#   }
-# }
